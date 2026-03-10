fc8495be9bc628a54a470f491d92033dc4b1d726
harsh moradiya <harshmoradiya05@gmail.com>
2026-03-10 18:48:43 +0530

    React v18 or React before 18

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -5,9 +5,13 @@ function App() {
   return <h1>Hello React!</h1>;
 }
 
+// React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+// React before 18
+// React.render(<App />,document.getElementById("root"))
